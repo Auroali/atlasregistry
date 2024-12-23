18032669b9a994cb0fb1cba860d5512517106d19
Auroali <dawn.auroali@gmail.com>
2024-12-23 14:44:39 -0400

    is now client only

--- src/test/java/com/auroali/atlasregistry/test/AtlasRegistryTest.java
+++ src/test/java/com/auroali/atlasregistry/test/AtlasRegistryTest.java
@@ -5,17 +5,12 @@ import com.auroali.altasregistry.v1.api.SpriteAtlasReference;
 import com.auroali.altasregistry.v1.api.SpriteAtlasReferenceBuilder;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AtlasRegistryTest implements ClientModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("atlasregistry-test");
     public static final SpriteAtlasReference TEST_ATLAS = SpriteAtlasReferenceBuilder.create(new Identifier("atlasregistry-test", "test"))
-            .reloadCallback((identifier, atlas) -> LOGGER.info("Reloaded atlas {}", atlas))
             .build();
     public static final SpriteAtlasReference OTHER_TEST_ATLAS = SpriteAtlasReferenceBuilder.create(new Identifier("atlasregistry-test", "test2"))
             .mipLevel(3)
-            .reloadCallback((identifier, atlas) -> LOGGER.info("Reloaded atlas {}", atlas))
             .build();
     @Override
     public void onInitializeClient() {
