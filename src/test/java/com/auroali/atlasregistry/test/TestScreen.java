e3cc33b8e532e97f5b3771bf8b9bf79b8756df83
Auroali <dawn.auroali@gmail.com>
2024-12-24 13:02:52 -0400

    fixed test

--- src/test/java/com/auroali/atlasregistry/test/TestScreen.java
+++ src/test/java/com/auroali/atlasregistry/test/TestScreen.java
@@ -8,19 +8,27 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class TestScreen extends Screen {
-    public static final Sprite ALTAS_TEST_SPRITE = AtlasRegistryTest.OTHER_TEST_ATLAS.getSprite(new Identifier("atlasregistry-test", "test2/test_texture2"));
-    public static final Sprite COPPER_BLOCK_SPRITE = VanillaAtlasReferences.BLOCKS.getSprite(new Identifier("block/copper_block"));
-    public static final Sprite DIAMOND_SPRITE = VanillaAtlasReferences.BLOCKS.getSprite(new Identifier("item/diamond"));
+    // can't declare these as static final because they wouldnt update on resourcepack reload
+    public Sprite atlasTest;
+    public Sprite copperBlock;
+    public Sprite diamond;
 
     protected TestScreen(Text title) {
         super(title);
     }
 
     @Override
+    protected void init() {
+        this.atlasTest = AtlasRegistryTest.OTHER_TEST_ATLAS.getSprite(new Identifier("atlasregistry-test", "test2/test_texture2"));
+        this.copperBlock = VanillaAtlasReferences.BLOCKS.getSprite(new Identifier("block/copper_block"));
+        this.diamond = VanillaAtlasReferences.BLOCKS.getSprite(new Identifier("item/diamond"));
+    }
+
+    @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
-        context.drawSprite(0, 0, 0, 32, 32, ALTAS_TEST_SPRITE);
-        context.drawSprite(32, 0, 0, 16, 16, COPPER_BLOCK_SPRITE);
-        context.drawSprite(48, 0, 0, 16, 16, DIAMOND_SPRITE);
+        context.drawSprite(0, 0, 0, 32, 32, this.atlasTest);
+        context.drawSprite(32, 0, 0, 16, 16, this.copperBlock);
+        context.drawSprite(48, 0, 0, 16, 16, this.diamond);
     }
 }
